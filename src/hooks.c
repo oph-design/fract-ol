4a4b387ed80cd5f3711cae32e5da073d76cc0b27
Ole-Paul Heinzelmann <oheinzel@1-E-5.42heilbronn.de>
2023-01-30 15:56:37 +0100

    2023-01-30 15:56:37

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 08:02:35 by oheinzel          #+#    #+#             */
-/*   Updated: 2023/01/24 18:43:27 by oheinzel         ###   ########.fr       */
+/*   Updated: 2023/01/30 15:54:08 by oheinzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,24 @@ void	color_shift(t_param *p, int factor)
 			p->color = 0;
 	if (p->color < 0 && factor < 0)
 			p->color = 16;
+	iterate(p);
+}
+
+void	shift_view(t_param *p, int key)
+{
+	if (key == 0)
+		p->midi = p->midi - 0.05 * p->zoom;
+	if (key == 1)
+		p->midr = p->midr - 0.05 * p->zoom;
+	if (key == 2)
+		p->midi = p->midi + 0.05 * p->zoom;
+	if (key == 3)
+		p->midr = p->midr + 0.05 * p->zoom;
+	if (key == 4)
+		p->it_max += 1;
+	if (key == 5)
+		p->it_max -= 1;
+	iterate(p);
 }
 
 void	loop_hook(void *param)
@@ -27,24 +45,23 @@ void	loop_hook(void *param)
 
 	p = param;
 	if (mlx_is_key_down(p->mlx, MLX_KEY_W))
-		p->midi = p->midi - 0.05 * p->zoom;
+		shift_view(p, 0);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_A))
-		p->midr = p->midr - 0.05 * p->zoom;
+		shift_view(p, 1);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_S))
-		p->midi = p->midi + 0.05 * p->zoom;
+		shift_view(p, 2);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_D))
-		p->midr = p->midr + 0.05 * p->zoom;
+		shift_view(p, 3);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_LEFT))
 		color_shift(p, 1);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_RIGHT))
 		color_shift(p, -1);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_UP))
-		p->it_max += 1;
+		shift_view(p, 4);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_DOWN))
-		p->it_max -= 1;
+		shift_view(p, 5);
 	if (mlx_is_key_down(p->mlx, MLX_KEY_ESCAPE))
 		ft_exit(p);
-	iterate(p);
 }
 
 void	my_scrollhook(double xdelta, double ydelta, void *param)
@@ -68,5 +85,6 @@ void	my_scrollhook(double xdelta, double ydelta, void *param)
 	calc_c(x, y, p);
 	p->midr += prevx - p->cords[0];
 	p->midi += prevy - p->cords[1];
-	iterate(p);
+	if (ydelta != 0)
+		iterate(p);
 }
